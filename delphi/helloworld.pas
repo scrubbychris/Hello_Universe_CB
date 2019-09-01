007e845f5b03d32dde56e0f01b36effea35a6a0b
scrubbychris <45634931+scrubbychris@users.noreply.github.com>
2019-09-01 16:48:07 -0400

    Add files via upload

---
+++ delphi/helloworld.pas
@@ -0,0 +1,34 @@
+unit helloworld;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Edit,
+  FMX.StdCtrls, FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    Label1: TLabel;
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Label1.Text := Edit1.Text;
+end;
+
+end.
